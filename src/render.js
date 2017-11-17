a7acc3478fb5b98096f7a3d79ec21e8c742c5644
Danny Lee <dxl1720@rit.edu>
2017-11-16 23:19:32 -0500

    Expanded Key Binds to Prompt

--- src/render.js
+++ src/render.js
@@ -8,56 +8,7 @@ let textarea, cWritebox, cSendbox, pWritebox, pSendbox;
 
  // Mechanics
 let connection = false;
-let userID;
-
- // Starts socket engine
-const serverConnect = function(){
-  if( connection ){ //If true, connection is already made
-    return;
-  }
-
-  // Prompt Setup
-  pWritebox = document.querySelector( '#prompt-writebox' );
-  pSendbox =  document.querySelector( '#prompt-sendbox'  );
-
-  const sendPrompt = function(){
-    const data = pWritebox.value;
-    socket.emit( 'join', data );
-  };
-
-  pSendbox.addEventListener( 'click', sendPrompt, false );
-
-  socket = io.connect();
-
-  socket.on( 'connect', function(){
-    connection = true;
-    document.querySelector( '#loader-wrapper' ).classList += "loaded";
-  });
-
-  socket.on( 'input-reprompt', function(){
-    pWritebox.value = "";
-    // TODO: User feedback that name already exists in active array
-  });
-
-  socket.on( 'start', function(){
-    console.log( "recieved" );
-    document.querySelector( '#prompt-wrapper' ).classList += "done";
-  });
-
-  socket.on( 'get-message', function( data ){
-    textarea.value += '\n' + data;
-  });
-};
-
-// Prompting server to send message to everyone in the room
-const postMessage = function(){
-  if( cWritebox.value ){
-    return;
-  }
-
-  socket.emit( 'post-message', { sender: userID, value: cWritebox.value } );
-  cWritebox.value = "";
-};
+let username;
 
 // Connects HTML Element references, setup dynamic CSS, setup event handlers
 const init = function(){
@@ -66,6 +17,8 @@ const init = function(){
   textarea  = document.querySelector( '#chatbox' );
   cWritebox = document.querySelector( '#chat-writebox' );
   cSendbox  = document.querySelector( '#chat-sendbox'  );
+  pWritebox = document.querySelector( '#prompt-writebox' );
+  pSendbox =  document.querySelector( '#prompt-sendbox'  );
 
   // Dynamic CSS value resets
   const resize = function(){
@@ -74,45 +27,81 @@ const init = function(){
              || document.body.clientWidth;
     textarea.style.width = width + "px";
   };
-
   resize();
   window.onresize = resize;
-
-  // Fix for browser tab duplicating
-  textarea.value = "";
-
-  // For enter key event
-  function handle( e ){
-    if( connection ){
-
-    } else {
-      // This causes any key to type into the chatbox without having to manually refocus after each enter press
-      writebox.focus();
-
-      if( e.keyCode == 13 ){
-        postMessage();
-      }
-    }
-  };
-
-  // Adding events for keyboard and pressing enter for the chat
   cSendbox.addEventListener( 'click', postMessage, false );
-  document.addEventListener( 'keypress', handle, false );
 
-  // Start socket engine
-  serverConnect();
+   // Clear fields
+   textarea.value  = "";
+   cWritebox.value = "";
+   pWritebox.value = "";
+
+  serverConnect(); // Start socket engine
 };
 
-/*
-const userIDprompt = function(){
-  let value = prompt( "Enter a username: ", "Your Username" );
-  if( !prompt ){
-    userIDprompt();
+ // Starts socket engine
+const serverConnect = function(){
+  if( connection ){ // No need to reconnect
+    return;
+  }
+
+  socket = io.connect();
+
+  socket.on( 'connect', function(){
+    document.querySelector( '#loader-wrapper' ).classList += "loaded";
+  });
+
+  const sendPrompt = function(){
+    const data = pWritebox.value;
+    socket.emit( 'join', data );
   };
+  pSendbox.addEventListener( 'click', sendPrompt, false );
 
-  userID = value;
-  init();
+  socket.on( 'input-reprompt', function(){
+    // TODO: User feedback that name already exists in active array
+  });
+
+  socket.on( 'start', function(){
+    connection = true;
+    username = pWritebox.value;
+    document.querySelector( '#prompt-wrapper' ).classList += "done";
+  });
+
+  socket.on( 'get-message', function( data ){
+    textarea.value += '\n' + data;
+  });
+
+  // After all setups are finished, finally set key binds
+  document.addEventListener( 'keypress', handleKeyPress, false );
 };
-*/
+
+// For enter key event
+const handleKeyPress = function( e ){
+  if( connection ){
+    // This causes any key to type into the chatbox without having to manually refocus after each enter press
+    cWritebox.focus();
+
+    if( e.keyCode == 13 ){ // enter key
+      postMessage();
+    }
+  } else {
+    pWritebox.focus();
+
+    if( e.keyCode == 13 ){ // enter key
+      const data = pWritebox.value;
+      socket.emit( 'join', data );
+    }
+  }
+};
+
+// Prompting server to send message to everyone in the room
+const postMessage = function(){
+  if( !cWritebox.value ){
+    return;
+  }
+
+  socket.emit( 'post-message', { sender: username, value: cWritebox.value } );
+  cWritebox.value = "";
+};
 
 window.onload = init;
